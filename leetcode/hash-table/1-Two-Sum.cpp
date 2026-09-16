f0596d8cd89f56ffc102f3a8d61b8c5d457725db
joffreyreed728495-svg <joffreyreed.728495@gmail.com>
2026-09-16 08:52:12 +0530

    Add Two-Sum (hash-table)

---
+++ leetcode/hash-table/1-Two-Sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int needed = target - nums[i];
+
+            // Check if the complement already exists
+            if (mp.find(needed) != mp.end()) {
+                return {mp[needed], i};
+            }
+
+            // Store current number and its index
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
